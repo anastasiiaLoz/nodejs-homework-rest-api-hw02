845ecb9cc88c600e88d14e2a36643a092a2dc1e9
Anastasiia Lozovych <anna.collana@gmail.com>
2021-07-16 17:44:58 +0100

    Adding changes in validation.js

--- validation/contacts.validation.js
+++ validation/contacts.validation.js
@@ -1,8 +1,21 @@
-exports.validate = (schema, reqPart = "body") => {
+exports.validateCreate = (schema, reqPart = "body") => {
   return (req, res, next) => {
     const result = schema.validate(req[reqPart]);
+    const message = "Missing required name field";
     if (result.error) {
-      return res.status(400).send(result.error);
+      return res.status(400).json({ message });
+    }
+
+    next();
+  };
+};
+
+exports.validateUpdate = (schema, reqPart = "body") => {
+  return (req, res, next) => {
+    const result = schema.validate(req[reqPart]);
+    const message = "Missing fields";
+    if (result.error) {
+      return res.status(400).json({ message });
     }
 
     next();
