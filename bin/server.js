1d8d86750c99f69f893f54b17b89b806eedc50a2
Anastasiia Lozovych <anna.collana@gmail.com>
2021-07-18 22:42:29 +0100

    ---

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,7 @@
 const app = require("../app");
+require("dotenv").config();
 
-const PORT = process.env.PORT || 4040;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
