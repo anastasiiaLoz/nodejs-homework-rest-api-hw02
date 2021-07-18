a8ce71833995f0852bc54f3c39397ddcf98b588b
Anastasiia Lozovych <anna.collana@gmail.com>
2021-07-18 22:50:01 +0100

    mn

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cowsay = require("cowsay");
 
-const contactsRouter = require("./routes/api/contacts");
+const mongodb = process.env.MONGODB_URI;
+mongoose.connect(
+  mongodb,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  },
+  function(err) {
+    if (err) {
+      console.log("Something went wrong. Database is not connected.");
+      process.exit(1);
+    }
+    console.log(
+      cowsay.say({
+        text: "Database connection successfull.",
+        e: "oO",
+        T: "U"
+      })
+    );
+  }
+);
+
+const contactsRouter = require("./routes/api/contacts.controller");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
