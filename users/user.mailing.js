680fa14d7ce8e2f472a13195048b0e4d93cf2ca9
Anastasiia Lozovych <anna.collana@gmail.com>
2021-07-29 19:01:39 +0100

    +++

--- users/user.mailing.js
+++ users/user.mailing.js
@@ -1,9 +1,7 @@
-// dotenv.config({ path: path.join(__dirname, ".env") });
-
 require("dotenv").config();
 const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey("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");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: "anastasiia.ucraniana@gmail.com",
   from: "anna.collana@gmail.com",
